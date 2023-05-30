45b3957bf9efaabdb12115f9c22da6fc253ea090
Kelf1729 <Kelfgh3@gmail.com>
2023-05-30 15:50:26 -0300

    ajuste final de perguntas script

--- js/script.js
+++ js/script.js
@@ -19,3 +19,17 @@ function ativarproduto(parametro) {
   }
 }
 parametros.forEach(ativarproduto);
+// Perguntas frequentes, efeito
+const perguntas = document.querySelectorAll(".perguntas button");
+function ativarpergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(controls);
+}
+function eventoperguntas(pergunta) {
+  pergunta.addEventListener("click", ativarpergunta);
+}
