50650e01fcc578a3e8446c650ac753e772e76526
Kelf1729 <Kelfgh3@gmail.com>
2023-05-30 14:14:41 -0300

    script de seguros e selcao

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Efeito de seleção de página
 // Selecionando os elementos
 const links = document.querySelectorAll(".header_menu a");
 // Função para cada link
@@ -9,3 +10,12 @@ function ativarlinks(link) {
   }
 }
 links.forEach(ativarlinks);
+// Enviar informação de bicicleta para orçamento
+const parametros = new URLSearchParams(location.search);
+function ativarproduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarproduto);
